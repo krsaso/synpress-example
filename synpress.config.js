0ec71bb2be18f3f9b96cef5634aa2ffbaa430d7a
Saso <saso@pantastic.com>
2023-10-05 14:10:55 +0200

    first commit

---
+++ synpress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('cypress');
+const synpressPlugins = require('@synthetixio/synpress/plugins');
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: 'https://highlight.xyz/',
+        specPattern: 'tests/e2e/specs',
+        supportFile: 'tests/support/index.js',
+        videosFolder: 'tests/e2e/videos',
+        screenshotsFolder: 'tests/e2e/screenshots',
+        video: false,
+        screenshotOnRunFailure: false,
+        setupNodeEvents(on, config) {
+            synpressPlugins(on, config);
+            return config
+        },
+    },
+});
+
+
+
+
---
+++ tests/e2e/specs/loginToMetamask.js
@@ -0,0 +1,51 @@
+// Define the test suite "Metamask Extension tests"
+describe('Metamask Extension tests', () => {
+
+    // Define the test case "connect to DApp with Metamask extension example"
+    it('connect to DApp with Metamask extension example', () => {
+
+        // Add a new network to Metamask using the `cy.addMetamaskNetwork()` command
+        cy.addMetamaskNetwork({
+            networkName: 'Optimism Goerli',
+            rpcUrl: 'https://goerli.optimism.io',
+            chainId: '420',
+            symbol: 'ETH',
+            blockExplorer: 'https://goerli-optimism.etherscan.io/',
+            isTestnet: true,
+        })
+
+        // Visit the root URL of the DApp
+        cy.visit('/');
+
+        // Click the "Connect" button on the DApp
+        cy.get('.mantine-qo1k2').eq(3).click();
+        cy.get('.iekbcc0').eq(42).click();
+
+        // Accept the Metamask access request by clicking the "Connect" button in the Metamask popup
+        cy.acceptMetamaskAccess().should('be.true');
+
+        // Assert that the "Connect" button on the DApp shows the text "Connected"
+        cy.get('[data-testid="rk-auth-message-button"]').click()
+
+    });
+
+    // Define the test case "create transaction example"
+    it('create transaction example', () => {
+
+        // Click the "Send" button on the DApp
+        cy.get('#sendButton').click();
+
+        // Confirm the Metamask transaction by clicking the "Confirm" button in the Metamask popup
+        cy.confirmMetamaskTransaction().should('be.true');
+    });
+
+    // Define the test case "confirm signature example"
+    it('confirm signature example', () => {
+
+        // Click the "Sign" button on the DApp
+        cy.get('#ethSign').click();
+
+        // Confirm the Metamask signature request by clicking the "Sign" button in the Metamask popup
+        cy.confirmMetamaskSignatureRequest().should('be.true');
+    });
+})
